e49ce4ed6dff06bbec49475f347cc903b58137be
mahady hasan rayhan <rayhan.feluda@gmail.com>
2022-02-02 11:23:43 +0900

    Add files via upload

---
+++ my_process_train.py
@@ -0,0 +1,23 @@
+import glob, os
+
+# Current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+print(current_dir)
+
+current_dir = '../given_data/images/train'
+
+# Percentage of images to be used for the test set
+percentage_test = 10;
+
+# Create and/or truncate train.txt and test.txt
+file_train = open('data/train.txt', 'w')
+
+# Populate train.txt and test.txt
+counter = 1
+index_test = round(100 / percentage_test)
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+
+    
+    file_train.write("data/obj" + "/" + title + '.jpg' + "\n")
